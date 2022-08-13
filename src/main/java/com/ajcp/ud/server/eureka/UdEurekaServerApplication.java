7f74448d563c2c9e9254d540897f537ad42f2ce3
andresjc15 <andres.jcp15@gmail.com>
2022-08-13 09:34:10 -0500

    primera carga

---
+++ src/main/java/com/ajcp/ud/server/eureka/UdEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ajcp.ud.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class UdEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajcp/ud/server/eureka/UdEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajcp.ud.server.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
